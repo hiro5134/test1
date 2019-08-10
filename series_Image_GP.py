cf2860080c835bbf3b041533095405b638da8d7a
八瀬辺洋人 <yasebehiroto@yasehenyouninnoMacBook-Air.local>
2019-08-10 15:01:42 +0900

    sum

--- series_Image_GP.py
+++ series_Image_GP.py
@@ -223,10 +223,10 @@ def main():
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
     stats_size = tools.Statistics(len)
     mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
-    mstats.register("avg", numpy.mean)
-    mstats.register("std", numpy.std)
-    mstats.register("min", numpy.min)
-    mstats.register("max", numpy.max)
+    mstats.register("avg", np.mean)
+    mstats.register("std", np.std)
+    mstats.register("min", np.min)
+    mstats.register("max", np.max)
 
     # 個体群と交叉率，突然変異率，エポック数を渡して進化計算実行
     pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 7, stats=mstats,
